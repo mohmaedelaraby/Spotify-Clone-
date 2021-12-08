9e32392f2e3d7d2171320c1c364975c9903b5e41
mohamedelaraby <elarabym857@gmail.com>
2021-12-09 01:14:07 +0200

    Updates

---
+++ precache-manifest.670cb1c407c8f86f4c92268fa223784a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bedc900040307f24a47553b2fcd4a1e",
+    "url": "/Spotify-Clone/index.html"
+  },
+  {
+    "revision": "8fa488dca8e57a33c0ae",
+    "url": "/Spotify-Clone/static/css/main.c2337694.chunk.css"
+  },
+  {
+    "revision": "5f68ffca7251c8a0ea40",
+    "url": "/Spotify-Clone/static/js/2.6fc08761.chunk.js"
+  },
+  {
+    "revision": "3f9558f3982572c5fb29a3aa309d2d82",
+    "url": "/Spotify-Clone/static/js/2.6fc08761.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8142d8dbdf35a276d789",
+    "url": "/Spotify-Clone/static/js/3.c1cc18e1.chunk.js"
+  },
+  {
+    "revision": "8fa488dca8e57a33c0ae",
+    "url": "/Spotify-Clone/static/js/main.028fa8f1.chunk.js"
+  },
+  {
+    "revision": "869a9939f20d4c6621eb",
+    "url": "/Spotify-Clone/static/js/runtime-main.5482a81a.js"
+  },
+  {
+    "revision": "f4313f2676d41021dee301d5ab674344",
+    "url": "/Spotify-Clone/static/media/Spotify-symbol.f4313f26.jpg"
+  },
+  {
+    "revision": "f91c02bdd03cd87eea8681bdaff1ad3e",
+    "url": "/Spotify-Clone/static/media/nirvana.f91c02bd.jpg"
+  }
+]);
